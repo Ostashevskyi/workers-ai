3785004eda51f7a2558617a27e68d23d53819eab
Yurii Ostashevskyi <xpeke2015@gmail.com>
2025-06-02 18:21:13 +0300

    t

--- app/api/generate_image/route.ts
+++ app/api/generate_image/route.ts
@@ -1,24 +1,23 @@
 import type { NextRequest } from "next/server";
 import { getRequestContext } from "@cloudflare/next-on-pages";
-import type { Env } from "@/app/types/cloudflare-env";
 
 export const runtime = "edge";
 
 export async function POST(request: NextRequest) {
-  const context = getRequestContext();
-  const { env } = context; 
-  const ai = env.AI
+//   const context = getRequestContext();
+//   const { env } = context; 
+//   const ai = env.AI
 
-  const body = await request.json();
+//   const body = await request.json();
 
-  let { prompt, model } = body as {prompt: string, model?: string};
-  if (!model) model = "@cf/black-forest-labs/flux-1-schnell";
+//   let { prompt, model } = body as {prompt: string, model?: string};
+//   if (!model) model = "@cf/black-forest-labs/flux-1-schnell";
 
-  const inputs = { prompt };
-  const res = await ai.run(model, inputs);
-  return new Response(`data:image/png;base64,${res.image}`, {
-    headers: {
-      "Content-Type": "image/png",
-    },
-  });
+//   const inputs = { prompt };
+//   const res = await ai.run(model, inputs);
+//   return new Response(`data:image/png;base64,${res.image}`, {
+//     headers: {
+//       "Content-Type": "image/png",
+//     },
+//   });
 }
